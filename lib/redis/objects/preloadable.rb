0cc0ffb2655e2713a185c71eee40829b00310698
kyohah <3257272+kyohah@users.noreply.github.com>
2026-02-20 04:40:07 +0900

    initial commit

---
+++ lib/redis/objects/preloadable.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "preloadable/version"
+
+module Redis
+  module Objects
+    module Preloadable
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/redis/objects/preloadable/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Redis
+  module Objects
+    module Preloadable
+      VERSION = "0.1.0"
+    end
+  end
+end
